7ba9a846f087163087c1b6b3812c3e9833557def
“Ayush <as8985719@gmail.com>
2021-11-29 10:55:45 +0530

    add test

---
+++ bridgelabz/FileUtils.java
@@ -0,0 +1,14 @@
+package bridgelabz;
+import java.io.File;
+public class FileUtils {
+	 /* This method is implemented to delete the existing files */
+    public static boolean deleteFiles(File contentsToDelete) {
+        File[] allContents = contentsToDelete.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteFiles(file);
+            }
+        }
+        return contentsToDelete.delete();
+    }
+}
---
+++ module-info.java
@@ -0,0 +1,3 @@
+module EmployeePayroll {
+	requires junit;
+}
